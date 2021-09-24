fa944cd0e5fd9ac013bedd51fa51ce1eb6fca7d6
_hange <52858353+onur-py@users.noreply.github.com>
2021-09-25 01:13:28 +0300

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,24 @@
+import string,random,os
+
+
+txt_name=input("txt name>>>")
+loop=int(input("loop>>>"))
+leng=int(input("password length>>>"))
+
+k=[string.ascii_lowercase,string.ascii_uppercase,string.digits,string.punctuation]
+
+txt=open(f"{txt_name}.txt","a+")
+
+
+for y in range(loop):
+	password=""
+	for i in range(leng):
+		value=random.choice(k)
+
+		password+=value[random.randint(0,len(value)-1)]
+	txt.write(password+"\n")
+	
+	print(password)
+
+os.startfile(f"{txt_name}.txt")
+txt.close()
